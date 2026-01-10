8440db4ae87e25d2897be27e09b54997c63a25f7
Rambihari Patel <rambiharipatel175@gmail.com>
2026-01-10 20:02:07 +0530

    footer section

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,21 +1,58 @@
-import React from 'react'
+import React from 'react';
 import Title from './Title'
 import assets from '../assets/assets'
+import toast from 'react-hot-toast';
+
+
 
 const ContactUs = () => {
+
+  
+
+  
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "74ccbfcc-6f26-44d4-ac35-4c3aa69da722");
+   
+    try{
+      const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      toast.success('Thanku for your submission')
+      event.target.reset();
+    } else {
+      toast.error(data.message)
+      
+    }
+
+    } catch(error) {
+      toast.error(error.message)
+    }
+
+    
+  };
+
+
   return (
     <div>
       <div id='contact-us' className='flex flex-col items-center gap-7 px-4 sm:px-12 
       lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'>
         <Title title= 'Reach out to us' desc='From strategy to execution, we craft digital solutions that move your business forward.'/>
        
-        <from className="grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full">
+        <form onSubmit = {onSubmit} className="grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full">
             <div>
                 <p className='mb-2 text-sm font-medium'>Your name</p>
                 <div className='flex pl-3 rounded-lg border border-gray-300
                 dark: border-gray-600'>
                     <img src={assets.person_icon} alt='' />
-                    <input type='text' placeholder='Enter your name' className='w-full p-3 text-sm outline-none' required />
+                    <input name='name' type='text' placeholder='Enter your name' className='w-full p-3 text-sm outline-none' required />
                 </div>
             </div>
 
@@ -24,13 +61,13 @@ const ContactUs = () => {
                 <div className='flex pl-3 rounded-lg border border-gray-300
                 dark: border-gray-600'>
                     <img src={assets.email_icon} alt='' />
-                    <input type='email' placeholder='Enter your email' className='w-full p-3 text-sm outline-none' required/>
+                    <input name='email' type='email' placeholder='Enter your email' className='w-full p-3 text-sm outline-none' required/>
                 </div>
             </div>
             
             <div className='sm:col-span-2'>
                 <p className='mb-2 text-sm font-medium'>Message</p>
-                <textarea rows={8} placeholder='Enter your message' className='w-full p-3 text-sm outline-none rounded-lg border border-gray-300 dark:border-gray-600' name='' id='' />
+                <textarea name = "message" required rows={8} placeholder='Enter your message' className='w-full p-3 text-sm outline-none rounded-lg border border-gray-300 dark:border-gray-600' id='' />
             </div>
 
             <button type='submit' className='w-max flex gap-2 bg-primary text-while text-sm px-10 py-3 rounded-full cursor-pointer hover:scale-103 transition-all'>
@@ -38,7 +75,7 @@ const ContactUs = () => {
                  <img src={assets.arrow_icon} alt='' className='w-4'/>
             </button>
 
-        </from>
+        </form>
 
       </div>
     </div>
